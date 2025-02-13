b1a0aa2a8fb6d93052b879cfc86c7faaa8a240ef
Brandon <kaiutoshan@gmail.com>
2025-02-13 08:42:54 -0800

    Project 15 Alpha

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,15 +1,13 @@
+const { ServerError } = require("../utils/errors");
+
 const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  console.log("Status Code:", err.statusCode);
-
-  if (err instanceof Error) {
-    return res.status(err.statusCode || 500).send({
-      message: err.message || "An unexpected error occurred",
-    });
+  if (err instanceof ServerError) {
+    return next(new ServerError("Internal Server Error"));
   }
 
-  res.status(500).send({ message: "Internal Server Error" });
+  return next(err);
 };
 
 module.exports = errorHandler;
