07911a991912c13bcbc89c9995a34b523a1af721
Brandon <kaiutoshan@gmail.com>
2025-02-14 07:07:46 -0800

    Project 15 Alpha

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,19 +1,5 @@
-// const { ServerError } = require("../utils/errors");
-
-// const errorHandler = (err, req, res, next) => {
-//   console.error(err);
-
-//   if (err instanceof ServerError) {
-//     return next(new ServerError("Internal Server Error"));
-//   }
-// };
-
-// module.exports = errorHandler;
-
 const errorHandler = (err, req, res, next) => {
   console.error(err);
-
-  // Use the statusCode from the error, default to 500 if not present
   const statusCode = err.statusCode || 500;
   const message = err.message || "An internal server error occurred";
 
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,7 @@
+const {
+  JWT_SECRET = "eSpDqVCW04V9SObFIaTmnkn3Kiv3mudBWNOMoqiedAkdMZxL4tl2K3bciyAdz350",
+} = process.env;
+
 module.exports = {
-  JWT_SECRET:
-    "eSpDqVCW04V9SObFIaTmnkn3Kiv3mudBWNOMoqiedAkdMZxL4tl2K3bciyAdz350",
+  JWT_SECRET,
 };
