76566d6c0406a5e005d5f256f657d84aa378ca23
Brandon <kaiutoshan@gmail.com>
2025-02-18 06:04:32 -0800

    Project 15 Submission 2

--- utils/config.js
+++ utils/config.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-console.log("JWT_SECRET:", JWT_SECRET);
-
-//const { SECRET_KEY = "JWT_SECRET" } = process.env;
-
 module.exports = {
   JWT_SECRET,
 };
